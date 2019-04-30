8c593379335bc35aaadb022b694743d8a1e31da3
Tu-username= <ivonne.morales.alarcon@gmail.com>
2019-04-30 17:25:48 -0500

    subiendo test

--- src/data.js
+++ src/data.js
@@ -1,110 +1,53 @@
 /* Manejo de data */
 
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-/*
-const example = () => {
-  return 'example';
+const Descendente = (datos) => {
+  const order = datos.sort((a, b) => {
+    if (a.yearOfBirth >= b.yearOfBirth) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+  return order;
 };
 
-window.example = example;
-*/
-/*
-const data = document.getElementById('data');
-function getData() {
-  fetch('https://raw.githubusercontent.com/Laboratoria/LIM009-DL-2.0/master/src/data/potter.json')
-    .then(res => res.json())
-    .then(dato => {
-      //console.log(dato);
-      data.innerHTML = `
-      <img src="${dato[1].image}" width="100px" class="img-fluid rounded-circle">
-      <p>Nombre:${dato[1].name} </p>
-      `
-    })
+const Ascendente = (datos) => {
+  const ordenar = datos.sort((a, b) => {
+    if (a.yearOfBirth <= b.yearOfBirth) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+  return ordenar;
 };
-*/
-/*
-const data = document.getElementById('data');
-function getData() {
-  fetch('https://raw.githubusercontent.com/Laboratoria/LIM009-DL-2.0/master/src/data/potter.json')
-    .then(res => res.json())
-    .then(datos => {
-      console.log(datos);
-      data.innerHTML = `<div>
-      <p>Nombre:${datos[0].name}</p>
-      <p>Actor:${datos[0].actor}</p>
-      <p>Casa:${datos[0].house}</p>
-      <div>
-        <img src= ${datos[0].image} />
-      </div>
-      </div>
-      
-      `
-      paintAll(datos)
-    })
+
+const filtrarGenero = (datos, genero) => {
+  const newGener = datos.filter(elemento => elemento.gender === genero);
+  return newGener;
 };
-*/
-/*
-function paint(datos){
-  data.innerHTML= '';
-  for(let i of datos){
-    console.log(i);
-    data.innerHTML += `
-          <tr>
-            <th scope='row'>${i.actor}</th>
-            <td>${i.name}</td>
-            <td>${i.gender}</td>
-            <td>${i.house}</td>
-          </tr>
-    `
-  }
-}
-*/
-const data = document.getElementById('data');
-function getData() {
-  fetch('https://raw.githubusercontent.com/Laboratoria/LIM009-DL-2.0/master/src/data/potter.json')
-    .then(data => data.json())
-    .then(data => {
-      console.log(data);
-      for (let i = 0; i < data.length; i++) {
-        let age = 2019 - data[i].yearOfBirth;
-        let ele = data[i];
-        let genero = data[i].gender;
-        paintAll(ele,age);
-        //filterData(genero);
-      }
-    });
-}
-
-function paintAll(ele,age) {
-  let all = `<div class="cards">
-      <figure class="cards-img"><img src= ${ele.image} class=""/></figure>
-      <div class="text"><p><strong>${ele.name}</strong></p>
-      <p>${ele.house ? ele.house : 'No tiene casa'}</p></div>
-      <div class="texto">
-      <p>Especie</br><strong>${ele.species}</strong></p>
-      <p>Rol</br><strong>${ele.hogwartsStudent ? 'Estudiante' : 'Staff'}</strong></p>
-      <p>Edad</br><strong>${age !== 2019 ? age : 'No tiene fecha de nacimiento'}</strong></p>
-      <p>Actor</br><strong>${ele.actor}</strong></p>
-      </div>
-      </div>
-    `
-  document.getElementById('data').innerHTML += all;
-}
 
 
- function filterData(genero) { 
-  if(genero !== 'male'){
-    return genero;
-  } else {
-    return genero;
-  }
-}
+const filtrarCasa = (datos, casa) => {
+  const newHouse = datos.filter(elemento => elemento.house === casa);
+  return newHouse;
+};
 
-const ordenarAZ = (a,b) => {
-  if (a.name > b.name) {
-    return 1;
-  } else {
-    return -1;
-  }
+const filtrarRolStudent = (datos) => {
+  const newStudent = datos.filter(elemento => elemento.hogwartsStudent);
+  return newStudent;
+};
+
+const filtrarRolStaff = (datos) => {
+  const newStaff = datos.filter(element => element.hogwartsStaff);
+  return newStaff;
+};
+
+window.potter = {
+  filtrarGenero,
+  filtrarCasa,
+  filtrarRolStudent,
+  filtrarRolStaff,
+  Descendente,
+  Ascendente
 };
--- src/main.js
+++ src/main.js
@@ -1,11 +1,32 @@
-/* Manejo del DOM */
-selectElement.addEventListener('change', (event) => {
-    const result = document.querySelector('.result');
-    result.textContent = `You like ${event.target.value}`;
+const descendOrder = (datos) => {
+  descendente.addEventListener('click', () => {
+    const orderDescend = window.potter.Ascendente(datos);
+    paintAll(orderDescend);
+  });
+};
 
+const filterGener = (datos) => {
+  genero.addEventListener('change', () => {
+    const paintGener = window.potter.filtrarGenero(datos, genero.value);
+    paintAll(paintGener);
+  });
+};
 
-    selectGenero.addEventListener('change', (event) => {
-        resetSelect(selectNamesElement);
-        const result = Pokemon.filtrarGenero(, 'type', element.target.value);
-        pintarNombresPrimeraPG(pokemons, pokemonesPrimeraPG);
-      });
+const filterHouse = (datos) => {
+  casa.addEventListener('change', () => {
+    const paintHouse = window.potter.filtrarCasa(datos, casa.value);
+    paintAll(paintHouse);
+  });
+};
+
+const filterRol = (datos) => {
+  rol.addEventListener('change', () => {
+    if (rol.value === "hogwartsStudent") {
+      const student = window.potter.filtrarRolStudent(datos, rol.value);
+      paintAll(student);
+    } else if (rol.value === "hogwartsStaff") {
+      const staff = window.potter.filtrarRolStaff(datos, rol.value);
+      paintAll(staff);
+    }
+  });
+};
